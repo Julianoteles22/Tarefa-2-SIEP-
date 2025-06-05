a9a469ff48239015ebe0f2c6791a0822abe04e67
Julianoteles22 <julianotelesabrahao@gmail.com>
2025-06-05 00:34:07 -0300

    Update dashboard_siep.py

--- dashboard_siep.py
+++ dashboard_siep.py
@@ -13,9 +13,9 @@ st.set_page_config(page_title="Dashboard - Ames Housing", layout="wide")
 # Logo da UnB + título e subtítulo
 col_logo, col_title = st.columns([1, 8])
 with col_logo:
-    st.image("Image (1).png", width=100)
+    st.image("Image (1).png", width=400)
 with col_title:
-    st.markdown("## Dashboard Interativo de Regressão e ANOVA - Ames Housing (com base embutida)")
+    st.markdown("## Dashboard Interativo de Regressão e ANOVA")
     st.markdown("### Alunos: Juliano Teles Abrahao (231013411) - João Pedro Lima de Carvalho (231013402)")
 
 
